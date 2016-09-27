93ba53aa929a32839f21f27c46394200ed4b404f
Gautham Ramachandran <gautham2r@gmail.com>
2016-09-27 17:00:19 +0530

    added role & user class

--- index.js
+++ index.js
@@ -11,38 +11,50 @@ let storage = new MongoStorage({
   url: 'mongodb://localhost:27017/acl',
   prefix: 'acl'
 });
+const ACL = require('./lib/acl');
 
 storage.on('ready', () => {
   console.log('Ready..', storage.isActive);
-  let add = Bluebird.coroutine(storage.add.bind(storage));
-  let get = Bluebird.coroutine(storage.get.bind(storage));
-  add(Enums.container.mongo.USER, {
-    user: cuid(),
-    role: cuid(),
-    isBlocked: false,
-    isActive: false
-  }).then(data => {
-    return get(Enums.container.mongo.USER, { user: data.user });
-  }).then((data) => {
-    return add(Enums.container.mongo.ROLE, {
-      _id: data.role,
-      name: 'Administrator'
-    });
-  }).then(data => {
-    return add(Enums.container.mongo.ACCESS, {
-      role: data._id,
-      resource: 'User',
-      read: true,
-      create: true,
-      delete: true,
-      update: true
-    });
-  }).catch(err => {
-    console.log(err);
-    console.log(err.stack);
-  }).finally(() => storage.clean());
+  let acl = new ACL(storage);
+  console.log(acl.role('Administrator').add());
+  acl.user('citjyi3ul0000c7gxri95i1uq').details().then(console.log);
+  return storage.clean();
+  // let add = Bluebird.coroutine(storage.add.bind(storage));
+  // let get = Bluebird.coroutine(storage.get.bind(storage));
+  // let remove = Bluebird.coroutine(storage.remove.bind(storage));
+  // add(Enums.container.mongo.USER, {
+  //   user: cuid(),
+  //   role: cuid(),
+  //   isBlocked: false,
+  //   isActive: false
+  // }).then(data => {
+  //   return get(Enums.container.mongo.USER, { user: data.user });
+  // }).then((data) => {
+  //   return add(Enums.container.mongo.ROLE, {
+  //     _id: data.role,
+  //     name: 'Administrator'
+  //   });
+  // }).then(data => {
+  //   return add(Enums.container.mongo.ACCESS, {
+  //     role: data._id,
+  //     resource: 'User',
+  //     read: true,
+  //     create: true,
+  //     delete: true,
+  //     update: true
+  //   });
+  // }).then(data => {
+  //   return remove(Enums.container.mongo.ACCESS, {
+  //     _id: data._id
+  //   }).then((result) => {
+  //     console.log('---------', result);
+  //   });
+  // }).catch(err => {
+  //   console.log(err);
+  //   console.log(err.stack);
+  // }).finally(() => storage.clean());
 });
 storage.on('clean', () => {
   console.log('closed');
 });
-storage.on('error', (err) => console.log('Error..', err));
+storage.on('error', (err) => console.log('Error..', err.stack));
